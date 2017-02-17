dc81b04ebb5eee55ad8607655fd291c0f797fc49
Tanner Welsh <tannerwelsh@gmail.com>
2017-02-17 17:59:06 -0500

    List exercises in README; include starter code

---
+++ src/guessTheNumber.js
@@ -0,0 +1,3 @@
+export default function guessTheNumber() {
+  // your code here
+}
---
+++ test/guessTheNumber_test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import guessTheNumber from '../src/guessTheNumber'
+
+describe('guessTheNumber()', () => {
+
+  // Your tests here...
+  
+})
