ee4dbd7b3cd728546938247075d7d3a8bc76b134
rogercamps <rogercamps@me.com>
2017-03-13 19:34:59 -0700

    write some tests guessTheNumber exercise

--- test/guessTheNumber_test.js
+++ test/guessTheNumber_test.js
@@ -3,6 +3,20 @@ import guessTheNumber from '../src/guessTheNumber'
 
 describe('guessTheNumber()', () => {
 
-  // Your tests here...
-  
+  it('should be a function', function(){
+      expect(guessTheNumber).to.be.a('function')
+    })
+
+  it('should generate a number between 1 and 100', function(){
+    expect(guessTheNumber(10)).to.equal(true)
+  })
+
+  it('should prompt user to input a number', function(){
+    exepct(guessTheNumber.prompt().to.equal('Please enter a number from 1 to 100.'))
+  })
+
+
+
+
+
 })
