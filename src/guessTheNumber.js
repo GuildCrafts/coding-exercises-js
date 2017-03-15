d7e481658d062cc2a29bf4e88a74ad2e294c23ae
rogercamps <rogercamps@me.com>
2017-03-14 17:44:46 -0700

    complete code for guessTheNumber and start to write tests

--- src/guessTheNumber.js
+++ src/guessTheNumber.js
@@ -1,3 +1,18 @@
 export default function guessTheNumber() {
-  // your code here
+  var randomNum = Math.floor(Math.random() * (100)) + 1
+  userInput = prompt("Guess a number between 1 and 100.\nTo stop playing type 'exit'.", "Write your number here")
+
+  while (userInput){
+    if (isNaN(userInput) && userInput !== "exit" ) {
+      var userInput = prompt("You can only use numbers between 1 and 100.", "Write your number here")
+    } else if (userInput == "exit") {
+      return alert("Try again later!")
+    } else if(randomNum > userInput) {
+      var userInput = prompt("Try a higher number!", "Write your number here")
+    } else if(randomNum < userInput) {
+      var userInput = prompt("Try a lower number!", "Write your number here")
+    } else {
+      return alert("You won!")
+    }
+  }
 }
--- test/guessTheNumber_test.js
+++ test/guessTheNumber_test.js
@@ -4,19 +4,37 @@ import guessTheNumber from '../src/guessTheNumber'
 describe('guessTheNumber()', () => {
 
   it('should be a function', function(){
-      expect(guessTheNumber).to.be.a('function')
-    })
+    expect(guessTheNumber()).to.be.a('function')
+  })
 
   it('should generate a number between 1 and 100', function(){
-    expect(guessTheNumber(10)).to.equal(true)
+  expect(guessTheNumber()).to.equal(true)
   })
 
-  it('should prompt user to input a number', function(){
-    exepct(guessTheNumber.prompt().to.equal('Please enter a number from 1 to 100.'))
+  xit('should prompt user to input a number', function(){
+  exepct(guessTheNumber.prompt().to.equal('Please enter a number from 1 to 100.'))
+  })
+  //not sure if we need a test for the prompt
+
+  xit('should check if the user input is an integer between 1 and 100', function(){
+    expect(/*userInput(10)*/).to.equal(true)
   })
 
+  xit('should return winning message if generated number is equal to the user input', function(){
+    expect(guessTheNumber(guess).alert).to.equal("the guess matched the computer's number")
+  })
+  xit('should return message if generated number is greater than the user input', function(){
+    expect(guessTheNumber(guess).alert).to.equal("the guess is lower than the computer's number")
+  })
 
+  xit('should return message if generated number is lower than the user input', function(){
+    expect(guessTheNumber(guess).alert).to.equal("the guess is greater than the computer's number")
 
+  })
 
+  xit("should return exit message and terminate the program if the user input is 'exit'", function(){
+    expect(guessTheNumber(guess).alert).to.equal("exit", /*terminate*/)
+
+  })
 
 })
