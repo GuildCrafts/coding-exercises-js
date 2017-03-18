b0e32f270734815d5bc4bccf330b6d3aa8db33a6
Leah Ann Mitchell <leahannmitchellmusic@gmail.com>
2017-03-17 17:06:28 -0700

    complete play it again exercise

--- src/playItAgain.js
+++ src/playItAgain.js
@@ -1,2 +1,24 @@
-var audioCtx = new AudioContext();
-console.log(audioCtx);
+var audioContext = new AudioContext();
+var oscillator = audioContext.createOscillator();
+var gainNode = audioContext.createGain();
+oscillator.connect(gainNode);
+gainNode.connect(audioContext.destination);
+oscillator.type = 'sine';
+var frequencies = [ 262,294,330,349,392,440,494,523 ];
+
+oscillator.start();
+function playSound(frequency){
+  oscillator.frequency.value = frequency;
+}
+
+let myNum = 2000
+frequencies.forEach( (value) => {
+  setTimeout(function(){
+    console.log(value)
+    playSound(value);
+  }, myNum+=2000)
+})
+
+ setTimeout(function(){
+    oscillator.disconnect()
+  }, myNum+=2000)
